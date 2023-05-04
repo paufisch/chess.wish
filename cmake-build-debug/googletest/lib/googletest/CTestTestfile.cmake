407575e7706eebb455b269a64584108371fa3887
Marcel Kant <makant@student.ethz.ch>
2023-05-04 10:54:31 +0200

    Test

---
+++ cmake-build-debug/googletest/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marcel/chess.wish/googletest/lib/googletest
+# Build directory: /home/marcel/chess.wish/cmake-build-debug/googletest/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
