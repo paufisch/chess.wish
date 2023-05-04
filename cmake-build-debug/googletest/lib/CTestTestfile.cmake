b0085f394c6652a2d73d4a91f07fc6db8262c7dd
jhofmaenner <jhofmaenner@student.ethz.ch>
2023-05-04 13:02:45 +0000

    Revert "Test"
    This reverts commit 407575e7706eebb455b269a64584108371fa3887

--- cmake-build-debug/googletest/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcel/chess.wish/googletest/lib
-# Build directory: /home/marcel/chess.wish/cmake-build-debug/googletest/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/googletest/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcel/chess.wish/googletest/lib/googletest
-# Build directory: /home/marcel/chess.wish/cmake-build-debug/googletest/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
