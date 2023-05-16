ca540a25b03a152d92cf8b65a0b88b38cec6bd54
Marcel Kant <makant@student.ethz.ch>
2023-05-16 16:18:07 +0200

    Structure of the piece classes not finished yet.

--- src/common/game_state/piece/pieces/rook.h
+++ src/common/game_state/piece/pieces/rook.h
@@ -7,12 +7,22 @@
 
 
 #include "../piece.h"
+#include <string>
+#include "../../../../../rapidjson/include/rapidjson/document.h"
 
 class rook : public piece{
+private:
+    std::vector<std::vector<bool>> _legal_moves;
+    Color _color;
+    std::string _piece_ID;
+
 public:
-    rook(std::string id, serializable_value<std::string>* piece_ID, serializable_value<std::string>* color);
+    rook(std::string id, std::string pieceId, Color color1, PieceType type, std::string piece_ID, Color color);
 
     std::vector<std::vector<bool>> legal_moves(unsigned row, unsigned col) override;
+
+    virtual void write_into_json(rapidjson::Value& json, rapidjson::Document::AllocatorType& allocator) const override;
+    static rook* from_json(const rapidjson::Value& json);
 };
 
 
