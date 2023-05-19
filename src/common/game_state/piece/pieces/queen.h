56ebb3f5f8e3e032486c7aec23192bb8950d7ec3
Marcel Kant <makant@student.ethz.ch>
2023-05-19 15:45:37 +0200

    Change in the subclasses.

--- src/common/game_state/piece/pieces/queen.h
+++ src/common/game_state/piece/pieces/queen.h
@@ -9,10 +9,18 @@
 #include "../piece.h"
 
 class Queen : public piece{
+private:
+
+    Queen(base_class_properties);
+
 public:
-    Queen(std::string id, serializable_value<std::string>* piece_ID, serializable_value<std::string>* color);
+
+    Queen(std::string piece_ID, Color color, PieceType type);
 
     std::vector<std::vector<bool>> legal_moves(unsigned row, unsigned col) override;
+
+    void write_into_json(rapidjson::Value& json, rapidjson::Document::AllocatorType& allocator) const override;
+    static Queen* from_json(const rapidjson::Value& json);
 };
 
 
