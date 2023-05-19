89d17569c62e6f26b1e9659655dc929b065cb1f5
Marcel Kant <makant@student.ethz.ch>
2023-05-19 20:43:46 +0200

    Added legal_moves to knight class

--- src/common/game_state/piece/pieces/knight.cpp
+++ src/common/game_state/piece/pieces/knight.cpp
@@ -33,8 +33,51 @@ Knight *Knight::from_json(const rapidjson::Value &json) {
     }
 }
 
-std::vector<std::vector<bool>> Knight::legal_moves(unsigned int row, unsigned int col) {
+void Knight::position(std::vector<std::vector<bool>>& possible_moves, unsigned int init_row, unsigned int init_col, int row_offset, int col_offset) {
+    if (_board->get_piece(init_row + row_offset, init_col + col_offset) != nullptr) {
+        piece* piece = _board->get_piece(init_row + row_offset, init_col + col_offset);
+        if (piece->get_color() != this->_color) {
+            possible_moves[init_row + row_offset][init_col + col_offset];
+        }
+    } else {
+        possible_moves[init_row + row_offset][init_col + col_offset];
+    }
+}
+
+std::vector<std::vector<bool>> Knight::legal_moves(unsigned int init_row, unsigned int init_col) {
     std::vector<std::vector<bool>> possible_moves(8, std::vector<bool>(8, false));
 
+    if (init_row < 5 && init_col < 7) {
+        position(possible_moves, init_row, init_col, 3, 1);
+    }
+
+    if (init_row < 5 && init_col > 0) {
+        position(possible_moves, init_row, init_col, 3, -1);
+    }
+
+    if (init_row > 2 && init_col < 7) {
+        position(possible_moves, init_row, init_col, -3, 1);
+    }
+
+    if (init_row > 2 && init_col > 0) {
+        position(possible_moves, init_row, init_col, -3, -1);
+    }
+
+    if (init_row < 7 && init_col < 5) {
+        position(possible_moves, init_row, init_col, 1, 3);
+    }
+
+    if (init_row > 0 && init_col < 5) {
+        position(possible_moves, init_row, init_col, -1, 3);
+    }
+
+    if (init_row < 7 && init_col > 2) {
+        position(possible_moves, init_row, init_col, 1, -3);
+    }
+
+    if (init_row > 0 && init_col > 2) {
+        position(possible_moves, init_row, init_col, -1, -3);
+    }
     return possible_moves;
 }
+
--- src/common/game_state/piece/pieces/knight.h
+++ src/common/game_state/piece/pieces/knight.h
@@ -15,12 +15,13 @@ class Knight : public piece{
 private:
 
     Knight(base_class_properties);
+    void position(std::vector<std::vector<bool>>& possible_moves, unsigned init_row, unsigned init_col, int row_offset, int col_offset);
 
 public:
 
     Knight(std::string piece_ID, Color color, PieceType type);
 
-    std::vector<std::vector<bool>> legal_moves(unsigned row, unsigned col) override;
+    std::vector<std::vector<bool>> legal_moves(unsigned init_row, unsigned init_col) override;
 
     void write_into_json(rapidjson::Value& json, rapidjson::Document::AllocatorType& allocator) const override;
     static Knight* from_json(const rapidjson::Value& json);
