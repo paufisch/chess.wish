693570a07b812a79b67423cbc2e941d60172c3ee
jhofmaenner <jhofmaenner@student.ethz.ch>
2023-05-19 02:12:13 +0200

    added player name to join_game_request

--- src/common/network/requests/join_game_request.cpp
+++ src/common/network/requests/join_game_request.cpp
@@ -14,27 +14,30 @@
 std::string join_game_request::undefined_game_id {""};
 
 // Public constructor
-join_game_request::join_game_request(std::string player_id)
-        : client_request( client_request::create_base_class_properties(RequestType::join_game, uuid_generator::generate_uuid_v4(), player_id, join_game_request::undefined_game_id) )
+join_game_request::join_game_request(std::string player_id, std::string name) :
+        client_request( client_request::create_base_class_properties(RequestType::join_game, uuid_generator::generate_uuid_v4(), player_id, join_game_request::undefined_game_id) ),
+        _player_name(name)
 { }
 
-join_game_request::join_game_request(std::string game_id, std::string player_id)
-        : client_request( client_request::create_base_class_properties(RequestType::join_game, uuid_generator::generate_uuid_v4(), player_id, game_id) )
+join_game_request::join_game_request(std::string game_id, std::string player_id, std::string name) :
+        client_request( client_request::create_base_class_properties(RequestType::join_game, uuid_generator::generate_uuid_v4(), player_id, game_id) ),
+        _player_name(name)
 { }
 
 // private constructor for deserialization
-join_game_request::join_game_request(client_request::base_class_properties props) :
-        client_request(props)
+join_game_request::join_game_request(client_request::base_class_properties props, std::string name) :
+        client_request(props), _player_name(name)
 { }
 
 void join_game_request::write_into_json(rapidjson::Value &json,
                                         rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator> &allocator) const {
     client_request::write_into_json(json, allocator);
+    json.AddMember("player_name", rapidjson::Value(_player_name, allocator), allocator);
 }
 
 join_game_request* join_game_request::from_json(const rapidjson::Value& json) {
     if (json.HasMember("player_name")) {
-        return new join_game_request(client_request::extract_base_class_properties(json));
+        return new join_game_request(client_request::extract_base_class_properties(json), json["player_name"].GetString());
     } else {
         throw LamaException("Could not parse join_game_request from json. player_name is missing.");
     }
