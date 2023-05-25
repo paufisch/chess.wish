712d463e1bc68c05b3a9bb87477affb33c4e70f3
fabian <fhauser@ethz.ch>
2023-05-25 22:06:53 +0200

    First adaptions inside piece_test

--- unit-tests/piece_test.cpp
+++ unit-tests/piece_test.cpp
@@ -7,55 +7,41 @@
 #include "gtest/gtest.h"
 #include "../src/common/exceptions/LamaException.h"
 #include "../src/common/game_state/piece/piece.h"
+#include "../src/common/game_state/board.h"
 #include "../src/common/serialization/json_utils.h"
 
+/* A test fixture allows to reuse the same configuration of objects for all
+ * tests in a test suite. The name of the fixture must match the test suite.
+ *
+ * For each test defined with TEST_F(), googletest will create a fresh test
+ * fixture at runtime, immediately initialize it via SetUp(), run the test,
+ * clean up by calling TearDown(), and then delete the test fixture.
+ * Note that different tests in the same test suite have different test fixture
+ * objects, and googletest always deletes a test fixture before it creates the
+ * next one. googletest does not reuse the same test fixture for multiple
+ * tests. Any changes one test makes to the fixture do not affect other tests.
+ */
+class Piece_Test : public ::testing::Test {
 
-// A card can be played on another card if and only if the new card has the
-// same value or the same value plus one than the previous card or the previous card
-// has value 7 and the new card has value 1.
-TEST(CardTest, PlayCardsOn1) {
-    card c_1(1);
-    card c_2(2);
-    card c_3(3);
-    card c_4(4);
-    card c_5(5);
-    card c_6(6);
-    card c_7(7);
-    EXPECT_TRUE(c_1.can_be_played_on((&c_1)));
-    EXPECT_TRUE(c_2.can_be_played_on((&c_1)));
-    EXPECT_TRUE(c_1.can_be_played_on((&c_7)));
+protected:
+    virtual void SetUp() {
+        cards.resize(8);
+        for (int i = 1; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                cards[i].push_back(new card(i));
+            }
+        }
+    }
 
-    EXPECT_FALSE(c_3.can_be_played_on((&c_1)));
-    EXPECT_FALSE(c_4.can_be_played_on((&c_1)));
-    EXPECT_FALSE(c_5.can_be_played_on((&c_1)));
-    EXPECT_FALSE(c_6.can_be_played_on((&c_1)));
-    EXPECT_FALSE(c_7.can_be_played_on((&c_1)));
+    /* Any object and subroutine declared here can be accessed in the tests */
 
-    EXPECT_FALSE(c_2.can_be_played_on((&c_7)));
-    EXPECT_FALSE(c_3.can_be_played_on((&c_7)));
-    EXPECT_FALSE(c_4.can_be_played_on((&c_7)));
-    EXPECT_FALSE(c_5.can_be_played_on((&c_7)));
-    EXPECT_FALSE(c_6.can_be_played_on((&c_7)));
+    // cards[i][j] holds a pointer to the j-th copy of a card of value i
+    std::vector<std::vector<card*>> cards;
+    hand player_hand;
+    std::string err;
+};
+
+TEST_F(Piece_Test, Pawn) {
+
 }
 
-// Serialization and subsequent deserialization must yield the same object
-TEST(CardTest, SerializationEquality) {
-    card card_send(1);
-    rapidjson::Document* json_send = card_send.to_json();
-    std::string message = json_utils::to_string(json_send);
-    delete json_send;
-
-    rapidjson::Document json_recv = rapidjson::Document(rapidjson::kObjectType);
-    json_recv.Parse(message.c_str());
-    card* card_recv = card::from_json(json_recv);
-    EXPECT_EQ(card_send.get_id(), card_recv->get_id());
-    EXPECT_EQ(card_send.get_value(), card_recv->get_value());
-    delete card_recv;
-}
-
-// Deserializing an invalid string must throw a LamaException
-TEST(CardTest, SerializationException) {
-    rapidjson::Document json = rapidjson::Document(rapidjson::kObjectType);
-    json.Parse("not json");
-    EXPECT_THROW(card::from_json(json), LamaException);
-}
